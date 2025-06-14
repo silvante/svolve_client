0abad36f4427765f9d78debb17fc765fc805e4e6
silvante <khamidov.ko@gmail.com>
2025-06-14 14:48:29 +0500

    Author title & home page

--- app/(global_components)/Author.tsx
+++ app/(global_components)/Author.tsx
@@ -3,16 +3,18 @@ import Link from "next/link";
 
 export default function Author() {
   return (
-    <Link href={"https://xamidov.uz"} target="_blanck" className="flex gap-1 justify-start items-center font-sans text_color">
-      <p>Created by</p>
-      <Image
-        src={"/images/author.jpg"}
-        alt="Authors picture"
-        width={37}
-        height={37}
-        className="rounded-full border border-gray-300"
-      />
-      <p>Mardonbek Khamidov</p>
-    </Link>
+    <div className="flex items-center justify-start">
+      <Link href={"https://xamidov.uz"} target="_blanck" className="flex gap-4 items-center justify-start">
+        <p>Created by</p>
+        <Image
+          src={"/images/author.jpg"}
+          alt="Authors picture"
+          width={37}
+          height={37}
+          className="rounded-full border border-gray-300"
+        />
+        <p>Mardonbek Khamidov</p>
+      </Link>
+    </div>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import Hero from "../(global_components)/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full py-10">
       <Hero />
     </div>
   );
