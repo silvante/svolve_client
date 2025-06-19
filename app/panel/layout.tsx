18612f34b0c47ffb015e1d105539ab0095c1941e
silvante <khamidov.ko@gmail.com>
2025-06-19 23:17:20 +0500

    panel layout

---
+++ app/panel/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "../globals.css";
+import Header from "../(global_components)/Header";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+});
+
+export const metadata: Metadata = {
+  title: "Svolve | Payment SaaS of Uzbekistan",
+  description: "Easy to use payment API for startups",
+  icons: {
+    icon: "/icons/icon.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.className} antialiased`}>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
