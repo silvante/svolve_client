359248a990d76fa4899137af9d4b159157e78088
silvante <khamidov.ko@gmail.com>
2025-07-08 14:32:37 +0500

    Organisations new form

--- app/panel/organisations/new/NewOrgForm.tsx
+++ app/panel/organisations/new/NewOrgForm.tsx
@@ -1,6 +1,17 @@
+"use client";
+
+import { REGEXP_ONLY_DIGITS } from "input-otp";
+
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+
 export default function NewOrganisationForm() {
   return (
-    <form>
+    <form className="space-y-5">
+      {/* name */}
       <div className="space-y-1">
         <label htmlFor="name" className="block">
           Organisation Name*
@@ -13,6 +24,37 @@ export default function NewOrganisationForm() {
           placeholder="Enter organisation name"
           required
         />
+      </div>
+
+      {/* pincode */}
+      <div className="space-y-1">
+        <label htmlFor="pincode" className="block">
+          Pincode*
+        </label>
+        <InputOTP
+          maxLength={6}
+          id="pincode"
+          pattern={REGEXP_ONLY_DIGITS}
+          required
+        >
+          <InputOTPGroup>
+            <InputOTPSlot index={0} className="border-gray-400" />
+            <InputOTPSlot index={1} className="border-gray-400" />
+            <InputOTPSlot index={2} className="border-gray-400" />
+            <InputOTPSlot index={3} className="border-gray-400" />
+            <InputOTPSlot index={4} className="border-gray-400" />
+            <InputOTPSlot index={5} className="border-gray-400" />
+          </InputOTPGroup>
+        </InputOTP>
+        <p className="text-sm text-gray-500">Number only</p>
+      </div>
+      <div>
+        <button
+          type="submit"
+          className="bg-violet-600 text-white py-2 px-5 rounded-md hover:bg-violet-700 transition-colors cursor-pointer"
+        >
+          Create Organisation
+        </button>
       </div>
     </form>
   );
