be63ca1253e8b35e76d91a46fdf33dd6de488bf3
silvante <khamidov.ko@gmail.com>
2025-07-10 14:34:57 +0500

    Org Breadcrubs

--- app/org/[unique_name]/(root)/page.tsx
+++ app/org/[unique_name]/(root)/page.tsx
@@ -1,11 +1,12 @@
 "use client";
+import Heading from "@/app/(global_components)/Heading";
 import { useSelector } from "react-redux";
 
 export default function UniqueNamePage() {
   const { unique_name } = useSelector((state: any) => state.validator);
   return (
     <div>
-      <h1>Home page</h1>
+      <Heading text="Home page"/>
     </div>
   );
 }
