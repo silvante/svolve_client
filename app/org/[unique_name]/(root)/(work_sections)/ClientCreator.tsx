260585ae61749d1926f50f2c124ece81dbb43849
silvante <khamidov.ko@gmail.com>
2025-07-14 17:03:21 +0500

    accordion

--- app/org/[unique_name]/(root)/(work_sections)/ClientCreator.tsx
+++ app/org/[unique_name]/(root)/(work_sections)/ClientCreator.tsx
@@ -1,7 +1,176 @@
+"use client";
+
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+const frameworks = [
+  {
+    value: "next.js",
+    label: "Next.js",
+  },
+  {
+    value: "sveltekit",
+    label: "SvelteKit",
+  },
+  {
+    value: "nuxt.js",
+    label: "Nuxt.js",
+  },
+  {
+    value: "remix",
+    label: "Remix",
+  },
+  {
+    value: "astro",
+    label: "Astro",
+  },
+];
+
 export default function ClientCreator() {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
   return (
-    <div>
-      <p>creator here</p>
+    <div className="w-full">
+      <form className="w-full">
+        <div className="grid grid-cols-3 gap-5">
+          <div className="flex flex-col space-y-1">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Clients name"
+              className="global_input"
+              required
+              autoFocus
+            />
+          </div>
+          <div className="flex flex-col space-y-1">
+            <label htmlFor="surname">Surname</label>
+            <input
+              type="text"
+              name="surname"
+              id="surname"
+              placeholder="Clients surname"
+              className="global_input"
+              required
+            />
+          </div>
+          <div className="flex flex-col space-y-1">
+            <label htmlFor="born_in">Year</label>
+            <input
+              type="number"
+              name="born_in"
+              id="born_in"
+              placeholder="Clients birth year"
+              className="global_input"
+              required
+            />
+          </div>
+          <div className="flex flex-col space-y-1">
+            <label htmlFor="origin">Origin</label>
+            <input
+              type="text"
+              name="origin"
+              id="origin"
+              placeholder="Client's hometown"
+              className="global_input"
+              required
+            />
+          </div>
+
+          <div className="flex flex-col space-y-1">
+            <label htmlFor="type">Type</label>
+            <Popover open={open} onOpenChange={setOpen}>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  role="combobox"
+                  aria-expanded={open}
+                  className="justify-between global_input w-full"
+                >
+                  <p>
+                    {value
+                      ? frameworks.find(
+                          (framework) => framework.value === value
+                        )?.label
+                      : "Select type..."}
+                  </p>
+                  <ChevronsUpDown className="opacity-50" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="p-0">
+                <Command>
+                  <CommandInput
+                    placeholder="Search type..."
+                    className="h-9"
+                  />
+                  <CommandList>
+                    <CommandEmpty>No framework found.</CommandEmpty>
+                    <CommandGroup>
+                      {frameworks.map((framework) => (
+                        <CommandItem
+                          key={framework.value}
+                          value={framework.value}
+                          onSelect={(currentValue) => {
+                            setValue(
+                              currentValue === value ? "" : currentValue
+                            );
+                            setOpen(false);
+                          }}
+                        >
+                          {framework.label}
+                          <Check
+                            className={cn(
+                              "ml-auto",
+                              value === framework.value
+                                ? "opacity-100"
+                                : "opacity-0"
+                            )}
+                          />
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </CommandList>
+                </Command>
+              </PopoverContent>
+            </Popover>
+          </div>
+          <div className="flex flex-col space-y-1">
+            <label htmlFor="price">Price</label>
+            <input
+              type="number"
+              name="price"
+              id="price"
+              placeholder="How much paid"
+              className="global_input"
+              required
+            />
+          </div>
+          <div className="flex flex-col space-y-1">
+            <button className="w-full h-full bg-violet-600 text-white p-2 rounded-xl">
+              Submit Client
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
