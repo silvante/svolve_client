4bfb986c89f5e27352e927645bbf7ce2e7a30fe9
silvante <khamidov.ko@gmail.com>
2025-07-21 23:52:05 +0500

    oops

--- app/org/[unique_name]/(root)/(work_sections)/(meta-components)/CheckClientBtn.tsx
+++ app/org/[unique_name]/(root)/(work_sections)/(meta-components)/CheckClientBtn.tsx
@@ -32,12 +32,12 @@ export default function CheckClientBtn({
   return (
     <button
       onClick={HandleCheckClient}
-      className="bg-violet-600 py-2 px-4 text-white rounded-xl"
+      className="bg-violet-600 py-2 px-4 text-white rounded-xl cursor-pointer"
     >
       {isLoading ? (
         "checking..."
       ) : (
-        <p className="flex gap-2 items-center cursor-pointer">
+        <p className="flex gap-2 items-center">
           Check <Check size={14} />
         </p>
       )}
