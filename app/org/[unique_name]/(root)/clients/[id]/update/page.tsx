cfc1bf0a4a24ac4d4c478fab1f061346f433dd52
silvante <khamidov.ko@gmail.com>
2025-07-22 20:00:08 +0500

    back btn

--- app/org/[unique_name]/(root)/clients/[id]/update/page.tsx
+++ app/org/[unique_name]/(root)/clients/[id]/update/page.tsx
@@ -1,10 +1,17 @@
+"use client";
 import Heading from "@/app/(global_components)/Heading";
 import UpdateClientForm from "./UpdateClientForm";
+import BackBtn from "@/app/(global_components)/BackBtn";
+import { useSelector } from "react-redux";
 
 export default function UpdateClient() {
+  const { organisation } = useSelector((state: any) => state.validator);
   return (
     <div className="space-y-5">
-      <Heading text="Update client" />
+      <div className="flex justify-between items-center">
+        <Heading text="Update Client" />
+        <BackBtn href={`/org/${organisation.unique_name}`} />
+      </div>
       <UpdateClientForm />
     </div>
   );
--- app/org/[unique_name]/(root)/types/[id]/update/page.tsx
+++ app/org/[unique_name]/(root)/types/[id]/update/page.tsx
@@ -2,12 +2,18 @@
 import Heading from "@/app/(global_components)/Heading";
 import { useParams } from "next/navigation";
 import UpdateTypeForm from "./UpdateTypeForm";
+import BackBtn from "@/app/(global_components)/BackBtn";
+import { useSelector } from "react-redux";
 
 export default function UpdateTypePage() {
   const { id } = useParams();
+  const { organisation } = useSelector((state: any) => state.validator);
   return (
     <div className="space-y-5">
-      <Heading text="Update Type" />
+      <div className="flex justify-between items-center">
+        <Heading text="Update Type" />
+        <BackBtn href={`/org/${organisation.unique_name}/types`} />
+      </div>
       <UpdateTypeForm />
     </div>
   );
