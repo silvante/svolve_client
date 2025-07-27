a57388882e4400b8a0d40d79f9f172195e18dacf
silvante <khamidov.ko@gmail.com>
2025-07-28 02:36:34 +0500

    Finally every thing is working, probably

--- app/org/(components)/OrgHeader.tsx
+++ app/org/(components)/OrgHeader.tsx
@@ -1,17 +1,22 @@
 "use client";
 import Svolve from "@/app/(global_components)/Svolve";
-import { useParams } from "next/navigation";
 import CollapseBtn from "./CollapseBtn";
 import LockOrgBtn from "./LockOrgBtn";
+import { useSelector } from "react-redux";
+import OrgLogo from "./OrgLogo";
 
 export default function OrgHeader() {
-  const { unique_name } = useParams();
+  const { organization } = useSelector((state: any) => state.validator);
   return (
     <header className="w-full bg-white p-3 border-b border-gray-200 flex justify-between items-center fixed top-0 left-0 z-50">
       <nav className="flex items-center justify-between w-full">
         <div className="flex items-center gap-4">
           <CollapseBtn />
-          <Svolve link={`/org/${unique_name}`} />
+          {!organization.logo ? (
+            <Svolve link={`/org/${organization.unique_name}`} />
+          ) : (
+            <OrgLogo />
+          )}
         </div>
         <div>
           <LockOrgBtn />
---
+++ app/org/(components)/OrgLogo.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useSelector } from "react-redux";
+
+export default function OrgLogo() {
+  const { organization } = useSelector((state: any) => state.validator);
+  if (!organization.logo) {
+    return;
+  }
+  return (
+    <Link href={`/org/${organization.unique_name}`} className="inline-block">
+      <Image
+        src={organization.logo}
+        alt="Logotype svolve"
+        width={0}
+        height={0}
+        className="w-auto h-10"
+      />
+    </Link>
+  );
+}
