3acaaed77dd37c1b1dc582fef6ccc05fd0c6efbb
silvante <khamidov.ko@gmail.com>
2025-08-02 23:17:53 +0500

    profile page danger zone

---
+++ app/panel/(root)/profile/(profilesections)/ProfileActions.tsx
@@ -0,0 +1,34 @@
+"use client";
+import Heading from "@/app/(global_components)/Heading";
+import { LogOut, UserPen } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+export default function ProfileActions() {
+  const router = useRouter();
+  function HandleLogOut() {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("reset_token");
+    window.location.reload();
+    router.push("/");
+  }
+  return (
+    <div className="p-8 flex-1 rounded-2xl special_shadowing w-full space-y-5">
+      <Heading text="Danger zone" />
+      <div className="flex gap-5">
+        <button
+          onClick={HandleLogOut}
+          className="bg-red-600/10 text-red-600 flex py-2 px-4 gap-2 items-center cursor-pointer rounded-lg hover:bg-red-600 hover:text-white transition-all"
+        >
+          <LogOut /> Log Out
+        </button>
+        <Link
+          href={"/signup"}
+          className="bg-gray-600/10 text-gray-900 flex py-2 px-4 gap-2 items-center cursor-pointer rounded-lg hover:bg-orange-600 hover:text-white transition-all"
+        >
+          <UserPen /> Switch Accounts
+        </Link>
+      </div>
+    </div>
+  );
+}
--- app/panel/(root)/profile/page.tsx
+++ app/panel/(root)/profile/page.tsx
@@ -3,6 +3,7 @@ import Heading from "@/app/(global_components)/Heading";
 import { Camera, KeyRound, Pen } from "lucide-react";
 import Link from "next/link";
 import { useSelector } from "react-redux";
+import ProfileActions from "./(profilesections)/ProfileActions";
 
 export default function ProfilePage() {
   const { currentUser } = useSelector((state: any) => state.user);
@@ -51,9 +52,7 @@ export default function ProfilePage() {
               <KeyRound size={50} />
             </Link>
             <div className="text-start">
-              <h2 className="text-3xl font-semibold text-gray-900">
-                Password
-              </h2>
+              <h2 className="text-3xl font-semibold text-gray-900">Password</h2>
               <p className="ext-gray-950">You password is secure!</p>
               <Link
                 href={"/panel/profile/settings"}
@@ -64,7 +63,7 @@ export default function ProfilePage() {
             </div>
           </div>
         </div>
-        <div className="p-10 flex-1 rounded-2xl special_shadowing w-full space-y-5">
+        <div className="p-8 flex-1 rounded-2xl special_shadowing w-full space-y-5">
           <div className="space-y-2 border-b border-b-gray-300 pb-3">
             <p className="text-lg text-gray-950 font-semibold">username:</p>
             <p className="text-gray-950">
@@ -105,6 +104,7 @@ export default function ProfilePage() {
             </p>
           </div>
         </div>
+        <ProfileActions />
       </div>
     </div>
   );
