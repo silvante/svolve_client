21281a4611e355ad4f9065f28c123a2097473004
silvante <khamidov.ko@gmail.com>
2025-08-12 14:32:16 +0500

    Search engine & pagination

--- app/org/[unique_name]/(root)/workers/employment/EmploymentSearchEngine.tsx
+++ app/org/[unique_name]/(root)/workers/employment/EmploymentSearchEngine.tsx
@@ -1,8 +1,10 @@
 "use client";
 import vacancyService from "@/app/api/services/vacancyService";
 import { origins } from "@/app/global/data";
-import { Organization } from "@/app/types/User";
+import { Organization, Vacancy } from "@/app/types/User";
 import { useEffect, useState } from "react";
+import VacancyCard from "./VacancyCard";
+import ErrorMessage from "@/app/(global_components)/ErrorMessage";
 
 export default function EmpSearchEngine({
   organization,
@@ -14,7 +16,7 @@ export default function EmpSearchEngine({
   const [origin, setOrigin] = useState(organization.origin);
   const [page, setPage] = useState(1);
   const [query, setQuery] = useState("");
-  const [meta, setMeta] = useState({ total: 0, page: 1, lastPage: 1 });
+  const [meta, setMeta] = useState({ total: 0, page: 1, last_page: 1 });
   const [vacancies, setVacancies] = useState([]);
 
   async function GetVacancies(e: any = null) {
@@ -48,43 +50,81 @@ export default function EmpSearchEngine({
   console.log(meta);
 
   return (
-    <div className="p-5 bg-white special_shadowing rounded-2xl space-y-5">
-      {error !== "" && (
-        <p className="text-red-600 bg-red-600/10 rounded-xl px-4 py-2">
-          {error}
-        </p>
-      )}
-      <form className="flex w-full gap-5" onSubmit={GetVacancies}>
-        <select
-          className="global_input w-40"
-          value={origin}
-          onChange={(e) => setOrigin(e.target.value)}
-        >
-          {origins.map((origin) => {
-            return (
-              <option key={origin.id} value={origin.name}>
-                {origin.name}
-              </option>
-            );
-          })}
-        </select>
-        <input
-          type="text"
-          name="query"
-          id="query"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          className="global_input flex-1"
-          placeholder="Search by name..."
-          required
+    <div className="space-y-8">
+      <div className="p-5 bg-white special_shadowing rounded-2xl space-y-5">
+        {error !== "" && (
+          <p className="text-red-600 bg-red-600/10 rounded-xl px-4 py-2">
+            {error}
+          </p>
+        )}
+        <form className="flex w-full gap-5" onSubmit={GetVacancies}>
+          <select
+            className="global_input w-40"
+            value={origin}
+            onChange={(e) => setOrigin(e.target.value)}
+          >
+            {origins.map((origin) => {
+              return (
+                <option key={origin.id} value={origin.name}>
+                  {origin.name}
+                </option>
+              );
+            })}
+          </select>
+          <input
+            type="text"
+            name="query"
+            id="query"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            className="global_input flex-1"
+            placeholder="Search by name..."
+          />
+          <button
+            className="py-2 w-40 text-center rounded-lg bg-violet-600 text-white cursor-pointer"
+            type="submit"
+          >
+            {isLoading ? "searching..." : "Search"}
+          </button>
+        </form>
+      </div>
+      {vacancies && vacancies.length > 0 ? (
+        <div className="w-full grid grid-cols-3 gap-5">
+          {vacancies.map((vacancy: Vacancy) => (
+            <VacancyCard key={vacancy.id} vacancy={vacancy} />
+          ))}
+        </div>
+      ) : (
+        <ErrorMessage
+          text="There is no vacancies found!"
+          desc="try something else"
         />
-        <button
-          className="py-2 w-40 text-center rounded-lg bg-violet-600 text-white cursor-pointer"
-          type="submit"
-        >
-          {isLoading ? "searching..." : "Search"}
-        </button>
-      </form>
+      )}
+      <div className="w-full flex justify-center items-center">
+        <div className="flex gap-5 justify-center items-center">
+          <button
+            className={`${
+              page <= 1 && "opacity-50"
+            } border-gray-300 border text-gray-800 py-2 px-4 rounded-full cursor-pointer hover:text-white hover:bg-violet-600 hover:border-violet-600 transition-all`}
+            disabled={page <= 1}
+            onClick={() => setPage(page - 1)}
+          >
+            Prev
+          </button>
+          <h3>
+            {page} / {meta.last_page}
+          </h3>
+          <button
+            className={`${
+              page >= meta.last_page && "opacity-50"
+            } border-gray-300 border text-gray-800 py-2 px-4 rounded-full cursor-pointer hover:text-white hover:bg-violet-600 hover:border-violet-600 transition-all`}
+            disabled={page >= meta.last_page}
+            onClick={() => setPage(page - 1)}
+          >
+            Next
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ app/org/[unique_name]/(root)/workers/employment/VacancyCard.tsx
@@ -0,0 +1,60 @@
+import { Vacancy } from "@/app/types/User";
+import { Camera, Eye } from "lucide-react";
+import Link from "next/link";
+
+export default function VacancyCard({ vacancy }: { vacancy: Vacancy }) {
+  return (
+    <div
+      className="rounded-xl border border-gray-200 shadow-md flex flex-col"
+    >
+      <div className="p-5 space-y-2 border-b border-gray-200">
+        <div className="bg-gray-300 max-w-16 w-full aspect-square rounded-full overflow-hidden border border-gray-400">
+          {vacancy.user.avatar ? (
+            <img
+              src={vacancy.user.avatar}
+              alt="Your avatar"
+              className="w-full h-full aspect-square object-cover"
+            />
+          ) : (
+            <div className="w-full h-full flex items-center justify-center text-gray-500">
+              <Camera size={18} />
+            </div>
+          )}
+        </div>
+        <div className="w-full">
+          <h2 className="text_color text-xl font-semibold w-full truncate">
+            {vacancy.name}
+          </h2>
+          <p className="text_color w-full truncate">{vacancy.user.email}</p>
+        </div>
+      </div>
+      <div className="p-5 space-y-1 border-b border-gray-200 w-full">
+        <p className="text_color font-medium w-full truncate">
+          Job: <span className="font-semibold">{vacancy.job}</span>
+        </p>
+        <p className="text_color font-medium text_clamp_3">
+          About: <span className="font-semibold">{vacancy.about}</span>
+        </p>
+        <p className="text_color font-medium w-full truncate">
+          Age: <span className="font-semibold">{vacancy.age}</span>
+        </p>
+      </div>
+      <div className="p-5 space-y-1 border-b border-gray-200 w-full">
+        <p className="text_color font-medium">
+          Contact:{" "}
+          <span className="font-semibold w-full truncate">
+            {vacancy.contact}
+          </span>
+        </p>
+      </div>
+      <div className="p-5 space-y-1 border-b border-gray-200 w-full flex items-center">
+        <Link
+          href={`/panel/vacancies/${vacancy.id}`}
+          className="flex gap-2 items-center bg-violet-600 text-white py-2 px-4 rounded-lg w-full justify-center"
+        >
+          <Eye /> See details
+        </Link>
+      </div>
+    </div>
+  );
+}
