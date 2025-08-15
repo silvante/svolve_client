c737e59791be7ef6dbadcaf6d3754bee54c6d9f2
silvante <khamidov.ko@gmail.com>
2025-08-15 21:45:54 +0500

    hiring

--- app/(auth)/(socials)/Socials.tsx
+++ app/(auth)/(socials)/Socials.tsx
@@ -2,17 +2,21 @@
 import { useState } from "react";
 import Github from "./(providers)/Github";
 import Google from "./(providers)/Google";
+import { ShieldAlert } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 export default function Socials() {
   const [errorMessage, setErrorMessage] = useState("");
 
   return (
-    <div className="text-center space-y-3">
-      <p>OR</p>
+    <div className="space-y-3">
+      <p className="text-center">OR</p>
       {errorMessage !== "" && (
-        <p className="text-red-600 bg-red-600/10 rounded-xl px-4 py-2 text-start">
-          {errorMessage}
-        </p>
+        <Alert variant="destructive">
+          <ShieldAlert />
+          <AlertTitle>Warning</AlertTitle>
+          <AlertDescription>{errorMessage}</AlertDescription>
+        </Alert>
       )}
       <div className="space-y-3">
         <Google setErrorMessage={setErrorMessage} />
