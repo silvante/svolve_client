c35a53a8a98b8aa965d84feae78b300aeaf565c4
silvante <khamidov.ko@gmail.com>
2025-09-03 21:23:27 +0500

    Just pfp

--- app/org/(components)/OrgHeader.tsx
+++ app/org/(components)/OrgHeader.tsx
@@ -4,11 +4,12 @@ import CollapseBtn from "./CollapseBtn";
 import LockOrgBtn from "./LockOrgBtn";
 import { useSelector } from "react-redux";
 import OrgLogo from "./OrgLogo";
+import OrgProfile from "./OrgProfile";
 
 export default function OrgHeader() {
   const { organization } = useSelector((state: any) => state.validator);
   return (
-    <header className="w-full bg-white p-3 border-b border-gray-300 flex justify-between items-center fixed top-0 left-0 z-50">
+    <header className="w-full bg-white px-3 py-2 border-b border-gray-300 flex justify-between items-center fixed top-0 left-0 z-50">
       <nav className="flex items-center justify-between w-full">
         <div className="flex items-center gap-4">
           <CollapseBtn />
@@ -18,8 +19,9 @@ export default function OrgHeader() {
             <OrgLogo />
           )}
         </div>
-        <div>
+        <div className="flex gap-4">
           <LockOrgBtn />
+          <OrgProfile />
         </div>
       </nav>
     </header>
---
+++ app/org/(components)/OrgProfile.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useSelector } from "react-redux";
+
+export default function OrgProfile() {
+  const { currentUser } = useSelector((state: any) => state.user);
+  if (!currentUser) {
+    return "Loading...";
+  }
+  
+  return (
+    <Avatar className="w-8 h-8  ">
+      <AvatarImage src={currentUser.avatar} />
+      <AvatarFallback>
+        {currentUser.name.split("")[0].toUpperCase()}
+      </AvatarFallback>
+    </Avatar>
+  );
+}
