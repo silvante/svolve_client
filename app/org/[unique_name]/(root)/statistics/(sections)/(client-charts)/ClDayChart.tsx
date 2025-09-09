ddd0bac18a0865cf8852788cbf113f8d80519189
silvante <khamidov.ko@gmail.com>
2025-09-09 12:49:14 +0500

    Client day chart

--- app/org/[unique_name]/(root)/statistics/(sections)/(client-charts)/ClDayChart.tsx
+++ app/org/[unique_name]/(root)/statistics/(sections)/(client-charts)/ClDayChart.tsx
@@ -1,10 +1,86 @@
-import Heading from "@/app/(global_components)/Heading";
-import { ClientsByDay } from "@/app/types/User";
+"use client";
 
-export default function ClDayChart({ data }: { data: ClientsByDay }) {
+import { Line } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+import { ClientsByDay } from "@/app/types/User";
+import { useState } from "react";
+import { BarChart3, LineChart } from "lucide-react";
+import Heading from "@/app/(global_components)/Heading";
+
+export default function RvDayChart({ data }: { data: ClientsByDay[] }) {
+  const [chartType, setChartType] = useState("line");
+
+  const labels = data.map((rbd) => rbd.day);
+  const clientsNumber = data.map((rbd) => rbd.count);
+
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Daily revenue",
+        data: clientsNumber,
+        borderColor: "#7f22fe",
+        backgroundColor: "#7f22fe",
+        fill: true,
+      },
+    ],
+  };
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: { position: "top" as const },
+    },
+  };
+
   return (
-    <div>
-      <p>Day chart</p>
+    <div className="p-5 space-y-5 rounded-2xl border border-gray-300 shadow-md w-full border-b-3 border-b-violet-600">
+      <Heading text="Last 30 days" />
+      <div className="flex gap-4">
+        <button
+          onClick={() => setChartType("line")}
+          className="border-gray-300 border rounded-lg py-2 px-4 flex gap-2 text_color hover:bg-gray-950/5 cursor-pointer"
+        >
+          <LineChart /> Line
+        </button>
+        <button
+          onClick={() => setChartType("bar")}
+          className="border-gray-300 border rounded-lg py-2 px-4 flex gap-2 text_color hover:bg-gray-950/5 cursor-pointer"
+        >
+          <BarChart3 /> Bar
+        </button>
+      </div>
+      <div className="w-full h-[400px]">
+        {chartType === "line" && (
+          <Line options={options} data={chartData} className="w-full h-full" />
+        )}
+        {chartType === "bar" && (
+          <Bar options={options} data={chartData} className="w-full h-full" />
+        )}
+      </div>
     </div>
   );
 }
