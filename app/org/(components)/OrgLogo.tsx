26704afa2322f71c5df3db85acaa47e18c5f2dd3
silvante <khamidov.ko@gmail.com>
2025-10-03 02:29:31 +0500

    payments: half of the way is done

--- app/org/(components)/OrgLogo.tsx
+++ app/org/(components)/OrgLogo.tsx
@@ -2,13 +2,16 @@ import Image from "next/image";
 import Link from "next/link";
 import { useSelector } from "react-redux";
 
-export default function OrgLogo() {
+export default function OrgLogo({ link }: { link?: string }) {
   const { organization } = useSelector((state: any) => state.validator);
   if (!organization.logo) {
     return;
   }
   return (
-    <Link href={`/org/${organization.unique_name}`} className="inline-block">
+    <Link
+      href={link ? link : `/org/${organization.unique_name}`}
+      className="inline-block"
+    >
       <Image
         src={organization.logo}
         alt="Logotype svolve"
--- app/org/[unique_name]/subscription/page.tsx
+++ app/org/[unique_name]/subscription/page.tsx
@@ -1,15 +1,105 @@
-export default async function ValidateOrganizationPage({
-  params,
-}: Readonly<{
-  params: { unique_name: string };
-}>) {
-  const { unique_name } = await params;
+"use client";
+
+import Spinner from "@/app/(global_components)/Spinner";
+import subscriptionService from "@/app/api/services/subscriptioService";
+import { Organization } from "@/app/types/User";
+import { useParams, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import BackBtn from "@/app/(global_components)/BackBtn";
+import Image from "next/image";
+
+export default function ValidateOrganizationPage() {
+  const { unique_name } = useParams();
+  const sParams = useSearchParams();
+  const page = sParams.get("page");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  // data
+  const [org, setOrg] = useState<null | Organization>(null);
+  const [url, setUrl] = useState<string | null>(null);
+
+  async function getCheckout() {
+    try {
+      const res: any = await subscriptionService.generateCheckout(
+        String(unique_name)
+      );
+      const response: { url: string; organization: Organization } = res;
+      console.log(res);
+      console.log(response);
+      setOrg(response.organization);
+      setUrl(response.url);
+      setError(null);
+    } catch (error: any) {
+      if (error.response && error.response.data) {
+        setError(
+          error.response.data.message ||
+            "An error occurred while validating the pincode."
+        );
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getCheckout();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center">
+        <p className="text-red-600 bg-red-600/10 rounded-xl px-4 py-2 text-center w-full">
+          {error}
+        </p>
+      </div>
+    );
+  }
+
+  console.log(url);
+  if (!org || !url) {
+    return;
+  }
+
   return (
-    <div className="w-full flex items-center justify-center">
-      <div className="main_body py-5">
-        <h1 className="text-xl">
-          Pay for <span className="font-semibold">@{unique_name}</span>
-        </h1>
+    <div className="w-full flex items-center justify-center h-screen">
+      <div className="w-full flex flex-col h-full">
+        <header className="w-full flex items-center justify-between p-5">
+          <div className="flex items-center gap-4">
+            {!org.logo ? (
+              <Image
+                src={"/icons/logo.svg"}
+                alt="Logotype svolve"
+                width={140}
+                height={38.5}
+              />
+            ) : (
+              <Image
+                src={org.logo}
+                alt="Logotype svolve"
+                width={0}
+                height={0}
+                className="w-auto h-10"
+              />
+            )}
+          </div>
+          <div>
+            <BackBtn href={page ? page : "/panel"} />
+          </div>
+        </header>
+        <iframe
+          src={url}
+          className="w-full flex-1 outline-none"
+          allow="payment"
+        />
       </div>
     </div>
   );
