ae47ea384e245d697a1f4bd2a4544832803aadc8
silvante <khamidov.ko@gmail.com>
2025-10-12 21:25:58 +0500

    Payment testing

--- app/api/api.config.ts
+++ app/api/api.config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://rebound-leaves-pace-stations.trycloudflare.com",
   headers: {
     Accept: "application/json",
   },
