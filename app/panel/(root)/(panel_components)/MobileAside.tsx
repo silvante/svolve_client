d01e1bd1fdf9e3b04a5e3d036a647a4e791ec68b
silvante <khamidov.ko@gmail.com>
2025-10-18 00:31:20 +0500

    res::someting is happening

--- app/panel/(root)/(panel_components)/MobileAside.tsx
+++ app/panel/(root)/(panel_components)/MobileAside.tsx
@@ -1,23 +1,30 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import {
   Sheet,
   SheetClose,
   SheetContent,
-  SheetDescription,
   SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { useRouter } from "next/navigation";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
+import AsideLink from "./(meta-components)/AsideLink";
+import {
+  CircleUser,
+  FileUser,
+  GitFork,
+  HandHeart,
+  House,
+  Settings,
+  WorkflowIcon,
+} from "lucide-react";
 
 export function MobileAside() {
-  const { currentUser, loading } = useSelector((state: any) => state.user);
-  const router = useRouter();
+//   const { currentUser, loading } = useSelector((state: any) => state.user);
+
   return (
     <Sheet>
       <SheetTrigger asChild className="block lg:hidden">
@@ -25,25 +32,51 @@ export function MobileAside() {
           <ProfileButton />
         </div>
       </SheetTrigger>
+
       <SheetContent>
         <SheetHeader>
-          <SheetTitle>Edit profile</SheetTitle>
-          <SheetDescription>
-            Make changes to your profile here. Click save when you&apos;re done.
-          </SheetDescription>
+          <SheetTitle>Navigation</SheetTitle>
         </SheetHeader>
-        <div className="grid flex-1 auto-rows-min gap-6 px-4">
-          <div className="grid gap-3">
-            <label htmlFor="sheet-demo-name">Name</label>
-            <Input id="sheet-demo-name" defaultValue="Pedro Duarte" />
-          </div>
-          <div className="grid gap-3">
-            <label htmlFor="sheet-demo-username">Username</label>
-            <Input id="sheet-demo-username" defaultValue="@peduarte" />
-          </div>
-        </div>
+
+        <aside className="p-2 flex flex-col gap-1">
+          <SheetClose asChild>
+            <AsideLink href="/panel">
+              <House /> Dashboard
+            </AsideLink>
+          </SheetClose>
+          <SheetClose asChild>
+            <AsideLink href="/panel/organizations">
+              <GitFork /> Organizations
+            </AsideLink>
+          </SheetClose>
+          <SheetClose asChild>
+            <AsideLink href="/panel/vacancies">
+              <FileUser /> Vacancies
+            </AsideLink>
+          </SheetClose>
+          <SheetClose asChild>
+            <AsideLink href="/panel/jobs">
+              <WorkflowIcon /> Jobs
+            </AsideLink>
+          </SheetClose>
+          <SheetClose asChild>
+            <AsideLink href="/panel/profile">
+              <CircleUser /> Profile
+            </AsideLink>
+          </SheetClose>
+          <SheetClose asChild>
+            <AsideLink href="/panel/community">
+              <HandHeart /> Community & support
+            </AsideLink>
+          </SheetClose>
+          <SheetClose asChild>
+            <AsideLink href="/panel/profile/settings">
+              <Settings /> Settings
+            </AsideLink>
+          </SheetClose>
+        </aside>
+
         <SheetFooter>
-          <Button type="submit">Save changes</Button>
           <SheetClose asChild>
             <Button variant="outline">Close</Button>
           </SheetClose>
