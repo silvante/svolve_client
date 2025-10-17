10f0a3f0823b47b7649cbfd67456bdf47bb7cbb5
silvante <khamidov.ko@gmail.com>
2025-10-17 23:01:21 +0500

    started to make responsive

--- app/lottie/DocsAnimation.tsx
+++ app/lottie/DocsAnimation.tsx
@@ -3,5 +3,5 @@ import Lottie from "lottie-react";
 import animation from "../assets/lottie/Doctors.json";
 
 export default function DocsAnimation() {
-  return <Lottie animationData={animation} className="w-2xl" />;
+  return <Lottie animationData={animation} className="w-full md:w-2xl" />;
 }
