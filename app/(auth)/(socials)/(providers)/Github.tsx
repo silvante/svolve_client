5c920b481d4c9906c93329763ee586e836ef8ba4
silvante <khamidov.ko@gmail.com>
2025-10-20 17:24:18 +0500

    GOOGLE and GITHUB

--- app/(auth)/(socials)/(providers)/Github.tsx
+++ app/(auth)/(socials)/(providers)/Github.tsx
@@ -1,40 +1,14 @@
 "use client";
-import authService from "@/app/api/services/authService";
-import { updateUser } from "@/app/store/slices/userSlice";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import toast from "react-hot-toast";
-import { useDispatch } from "react-redux";
+import { useState } from "react";
 
 export default function Github({ setErrorMessage }: any) {
-  const router = useRouter();
-  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
 
   const handleGithubLogin = () => {
     try {
+      setLoading(true);
       window.location.href = "http://localhost:8080/auth/github";
-
-      const receiveMessage = async (event: MessageEvent) => {
-        if (event.origin !== "http://localhost:8080") return;
-
-        if (event.data.reset_token && event.data.access_token) {
-          localStorage.setItem("access_token", event.data.access_token);
-          localStorage.setItem("reset_token", event.data.reset_token);
-        }
-        if (event.data.is_ok == false) {
-          return setErrorMessage(
-            event.data.message
-              ? event.data.message
-              : "Something went wrong, please try again later"
-          );
-        }
-        // toast.success("Registered successfully");
-        const user = await authService.getProfile();
-        dispatch(updateUser(user));
-        router.push("/panel");
-      };
-
-      window.addEventListener("message", receiveMessage, false);
     } catch (error) {
       setErrorMessage("something went wrong, please try again later");
     }
@@ -43,7 +17,10 @@ export default function Github({ setErrorMessage }: any) {
   return (
     <button
       onClick={handleGithubLogin}
-      className="flex gap-3 cursor-pointer w-full border border-gray-400 p-3 rounded-xl items-center justify-center"
+      className={`flex gap-3 cursor-pointer w-full border border-gray-400 p-3 rounded-xl items-center justify-center ${
+        loading ? "opacity-70" : ""
+      }`}
+      disabled={loading}
     >
       <Image
         src={"/social_icons/GitHub.svg"}
--- app/(auth)/(socials)/(providers)/Google.tsx
+++ app/(auth)/(socials)/(providers)/Google.tsx
@@ -1,47 +1,26 @@
 "use client";
-import authService from "@/app/api/services/authService";
-import { updateUser } from "@/app/store/slices/userSlice";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useDispatch } from "react-redux";
+import { useState } from "react";
 
 export default function Google({ setErrorMessage }: any) {
-  const router = useRouter();
-  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
 
   const handleGoogleLogin = () => {
     try {
+      setLoading(true);
       window.location.href = "http://localhost:8080/auth/google";
-
-      const receiveMessage = async (event: MessageEvent) => {
-        if (event.origin !== "http://localhost:8080") return;
-
-        if (event.data.reset_token && event.data.access_token) {
-          localStorage.setItem("access_token", event.data.access_token);
-          localStorage.setItem("reset_token", event.data.reset_token);
-        }
-        if (event.data.is_ok == false) {
-          return setErrorMessage(
-            event.data.message
-              ? event.data.message
-              : "Something went wrong, please try again later"
-          );
-        }
-        // toast.success("Registered successfully");
-        const user = await authService.getProfile();
-        dispatch(updateUser(user));
-        router.push("/panel");
-      };
-
-      window.addEventListener("message", receiveMessage, false);
     } catch (error) {
       setErrorMessage("Something went wrong, please try again later");
     }
   };
+
   return (
     <button
       onClick={handleGoogleLogin}
-      className="flex gap-3 cursor-pointer w-full border border-gray-400 p-3 rounded-xl items-center justify-center"
+      className={`flex gap-3 cursor-pointer w-full border border-gray-400 p-3 rounded-xl items-center justify-center ${
+        loading ? "opacity-70" : ""
+      }`}
+      disabled={loading}
     >
       <Image
         src={"/social_icons/Google.svg"}
