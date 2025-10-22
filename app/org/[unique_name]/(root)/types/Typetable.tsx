731eda2b34368fa2209e23143c78ac0b101b6c33
silvante <khamidov.ko@gmail.com>
2025-10-23 01:25:58 +0500

    res::org_type_page

--- app/org/[unique_name]/(root)/types/Typetable.tsx
+++ app/org/[unique_name]/(root)/types/Typetable.tsx
@@ -86,191 +86,74 @@ export default function TypeTable() {
               </div>
             )}
 
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-              <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <div className="relative overflow-x-auto shadow-md rounded-lg border border-gray-300">
+              <table className="w-full text-sm text-left rtl:text-right text-gray-800 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b border-gray-300">
                   <tr>
-                    <th scope="col" className="px-6 py-3">
-                      Product name
+                    <th scope="col" className="px-6 py-3 whitespace-nowrap">
+                      o/n
                     </th>
-                    <th scope="col" className="px-6 py-3">
-                      Color
+                    <th scope="col" className="px-6 py-3 whitespace-nowrap">
+                      Name
                     </th>
-                    <th scope="col" className="px-6 py-3">
-                      Category
+                    <th scope="col" className="px-6 py-3 whitespace-nowrap">
+                      Description
                     </th>
-                    <th scope="col" className="px-6 py-3">
+                    <th scope="col" className="px-6 py-3 whitespace-nowrap">
                       Price
                     </th>
-                    <th scope="col" className="px-6 py-3">
+                    <th scope="col" className="px-6 py-3 whitespace-nowrap">
+                      Total
+                    </th>
+                    <th scope="col" className="px-6 py-3 whitespace-nowrap">
                       Action
                     </th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      Apple MacBook Pro 17"
-                    </th>
-                    <td className="px-6 py-4">Silver</td>
-                    <td className="px-6 py-4">Laptop</td>
-                    <td className="px-6 py-4">$2999</td>
-                    <td className="px-6 py-4">
-                      <a
-                        href="#"
-                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  {types.map((type: Type, index: number) => (
+                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+                      <th
+                        scope="row"
+                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                       >
-                        Edit
-                      </a>
-                    </td>
-                  </tr>
-                  <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      Microsoft Surface Pro
-                    </th>
-                    <td className="px-6 py-4">White</td>
-                    <td className="px-6 py-4">Laptop PC</td>
-                    <td className="px-6 py-4">$1999</td>
-                    <td className="px-6 py-4">
-                      <a
-                        href="#"
-                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                      >
-                        Edit
-                      </a>
-                    </td>
-                  </tr>
-                  <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      Magic Mouse 2
-                    </th>
-                    <td className="px-6 py-4">Black</td>
-                    <td className="px-6 py-4">Accessories</td>
-                    <td className="px-6 py-4">$99</td>
-                    <td className="px-6 py-4">
-                      <a
-                        href="#"
-                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                      >
-                        Edit
-                      </a>
-                    </td>
-                  </tr>
-                  <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      Google Pixel Phone
-                    </th>
-                    <td className="px-6 py-4">Gray</td>
-                    <td className="px-6 py-4">Phone</td>
-                    <td className="px-6 py-4">$799</td>
-                    <td className="px-6 py-4">
-                      <a
-                        href="#"
-                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                      >
-                        Edit
-                      </a>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      Apple Watch 5
-                    </th>
-                    <td className="px-6 py-4">Red</td>
-                    <td className="px-6 py-4">Wearables</td>
-                    <td className="px-6 py-4">$999</td>
-                    <td className="px-6 py-4">
-                      <a
-                        href="#"
-                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                      >
-                        Edit
-                      </a>
-                    </td>
-                  </tr>
+                        {index + 1}
+                      </th>
+                      <td className="px-6 py-4 whitespace-nowrap">{type.name}</td>
+                      <td className="px-6 py-4 whitespace-nowrap">{type.description}</td>
+                      <td className="px-6 py-4 whitespace-nowrap">{type.price} uzs</td>
+                      <td className="px-6 py-4 whitespace-nowrap">{type._count.clients} clients</td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <DropdownMenu>
+                          <DropdownMenuTrigger className="flex gap-2 bg-violet-600 px-1 py-1 text-white font-semibold rounded-md hover:bg-violet-700 transition-colors">
+                            <Menu />
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent align="end" className="w-56">
+                            <OrgLink
+                              href={`/types/${type.id}/update`}
+                              className="rounded-lg flex"
+                            >
+                              <DropdownMenuItem className="cursor-pointer w-full">
+                                <PenBox /> Update
+                              </DropdownMenuItem>
+                            </OrgLink>
+                            <button
+                              className="rounded-lg cursor-pointer w-full"
+                              onClick={() => HandleDelete(type.id)}
+                            >
+                              <DropdownMenuItem className="cursor-pointer">
+                                <Trash color="#e7000b" />{" "}
+                                <p className="text-red-600">Delete</p>
+                              </DropdownMenuItem>
+                            </button>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
-
-            {/* <table className="w-full rounded-lg overflow-hidden">
-              <thead className="bg-gray-200">
-                <tr className="">
-                  <th className="text-start p-3">No</th>
-                  <th className="text-start p-3">Name</th>
-                  <th className="text-start p-3">Description</th>
-                  <th className="text-start p-3">Price</th>
-                  <th className="text-start p-3">Total</th>
-                  <th className="text-start p-3">Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {types.map((type: Type, index: number) => (
-                  <tr
-                    key={type.id}
-                    className={`border-b border-x border-gray-200`}
-                  >
-                    <td className="p-3 w-20 truncate">{index + 1}</td>
-                    <td className="p-3 truncate">{type.name}</td>
-                    <td className="p-3 truncate">{type.description}</td>
-                    <td className="p-3 truncate">{type.price}</td>
-                    <td className="p-3 truncate">
-                      {type._count.clients} clients
-                    </td>
-                    <td className="p-3 truncate flex gap-4">
-                      <DropdownMenu>
-                        <DropdownMenuTrigger className="flex gap-2 bg-violet-600 px-1 py-1 text-white font-semibold rounded-md hover:bg-violet-700 transition-colors">
-                          <Menu />
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end" className="w-56">
-                          <OrgLink
-                            href={`/types/${type.id}/update`}
-                            className="rounded-lg flex"
-                          >
-                            <DropdownMenuItem className="cursor-pointer w-full">
-                              <PenBox /> Update
-                            </DropdownMenuItem>
-                          </OrgLink>
-                          <button
-                            className="rounded-lg cursor-pointer w-full"
-                            onClick={() => HandleDelete(type.id)}
-                          >
-                            <DropdownMenuItem className="cursor-pointer">
-                              <Trash color="#e7000b" />{" "}
-                              <p className="text-red-600">Delete</p>
-                            </DropdownMenuItem>
-                          </button>
-                        </DropdownMenuContent>
-                      </DropdownMenu>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-              <tfoot className="bg-gray-200">
-                <tr>
-                  <th className="p-3"></th>
-                  <th className="p-3"></th>
-                  <th className="p-3"></th>
-                  <th className="p-3"></th>
-                  <th className="p-3"></th>
-                  <th className="p-3"></th>
-                </tr>
-              </tfoot>
-            </table> */}
           </div>
         ) : (
           <ErrorMessage
