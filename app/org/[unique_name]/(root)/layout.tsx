aa879b793daff9aa15abd874283db5bcaa6006ee
silvante <khamidov.ko@gmail.com>
2025-10-23 00:23:50 +0500

    res::TF

--- app/org/[unique_name]/(root)/layout.tsx
+++ app/org/[unique_name]/(root)/layout.tsx
@@ -45,11 +45,11 @@ export default function OrgLayout({
           <OrgHeader />
           <main className="w-full flex min-h-screen">
             <OrgAside />
-            <div className="flex-1 pt-20 px-5 max-w-[1480px] mx-auto space-y-5 pb-10 lg:pb-5 flex flex-col">
-              <div className="w-full flex items-center justify-start">
+            <div className="flex-1 pt-20 px-5 max-w-[1480px] mx-auto pb-14 lg:pb-5 flex flex-col">
+              <div className="w-full flex items-center justify-start mb-5">
                 <OrgBreadcrumbs />
               </div>
-              <div className="flex-1">{children}</div>
+              <div className="flex-1 mb-5">{children}</div>
               <OrgFooter />
               <MobileNav />
             </div>
