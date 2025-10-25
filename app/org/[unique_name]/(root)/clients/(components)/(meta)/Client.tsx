d167c6d919e3374f46dbbe011daa5eb1ba8603ed
silvante <khamidov.ko@gmail.com>
2025-10-25 23:24:01 +0500

    Client with diagnosis is ready

--- app/org/[unique_name]/(root)/clients/(components)/(meta)/Client.tsx
+++ app/org/[unique_name]/(root)/clients/(components)/(meta)/Client.tsx
@@ -1,31 +1,43 @@
 import { Client } from "@/app/types/User";
 
 export default function ClientCard({ client }: { client: Client }) {
+  const date = new Date(client.created_at);
   return (
-    <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between w-full border rounded-xl px-4 py-2 shadow-sm bg-white hover:shadow-md transition">
-      <div className="flex flex-wrap items-center gap-2">
-        <span className="font-semibold text-gray-800">
-          {client.name} {client.surname}
-        </span>
-        <span className="text-sm text-gray-500">
-          ({client.born_in}, {client.origin})
-        </span>
-        <span className="text-sm text-violet-600 font-medium">
-          {client.type.name} – {client.type.description}
-        </span>
-      </div>
+    <div className="flex flex-col border rounded-xl overflow-hidden bg-white shadow-sm hover:shadow-md">
+      <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between w-full border-gray-300 px-4 py-2 border-b transition">
+        <div className="flex flex-wrap items-center gap-2">
+          <span className="font-semibold text-gray-800">
+            {client.name} {client.surname}
+          </span>
+          <span className="text-sm text-gray-500">
+            ({client.born_in}, {client.origin})
+          </span>
+          <span className="text-sm text-violet-600 font-medium">
+            {client.type.name} – {client.type.description}
+          </span>
+        </div>
 
-      <div className="flex items-center gap-3 mt-2 sm:mt-0">
-        <span className="text-green-600 font-bold">{client.price} UZS</span>
-        {client.is_checked ? (
-          <span className="px-2 py-0.5 text-xs rounded-full bg-green-100 text-green-600">
-            Checked
-          </span>
-        ) : (
-          <span className="px-2 py-0.5 text-xs rounded-full bg-red-100 text-red-600">
-            Pending
-          </span>
-        )}
+        <div className="flex items-center gap-3 mt-2 sm:mt-0">
+          <span className="text-green-600 font-bold">{client.price} UZS</span>
+          {client.is_checked ? (
+            <span className="px-2 py-0.5 text-xs rounded-full bg-green-100 text-green-600">
+              Checked
+            </span>
+          ) : (
+            <span className="px-2 py-0.5 text-xs rounded-full bg-red-100 text-red-600">
+              Pending
+            </span>
+          )}
+        </div>
+      </div>
+      <div className="text_color px-4 py-2 space-y-2">
+        <p>
+          <span className="font-semibold">visited on:</span>{" "}
+          {date.toLocaleDateString()}
+        </p>
+        <p>
+          <span className="font-semibold">diagnosis:</span> {client.diagnosis}
+        </p>
       </div>
     </div>
   );
