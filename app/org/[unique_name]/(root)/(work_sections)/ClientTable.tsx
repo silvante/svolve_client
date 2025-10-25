4fb7cdbfb8e2979293e57b0f4e2ed7fcbffc86d9
silvante <khamidov.ko@gmail.com>
2025-10-25 17:40:54 +0500

    form tr

--- app/org/[unique_name]/(root)/(work_sections)/ClientTable.tsx
+++ app/org/[unique_name]/(root)/(work_sections)/ClientTable.tsx
@@ -232,7 +232,7 @@ export default function ClientTable() {
                         </tr>
 
                         <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-                          <td>app</td>
+                          <td className="px-6 py-4 whitespace-nowrap">app</td>
                         </tr>
                       </React.Fragment>
                     ))}
