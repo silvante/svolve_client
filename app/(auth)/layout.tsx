8e7f31c55e236283c21c6bd19799d660137c6fb9
silvante <khamidov.ko@gmail.com>
2025-11-06 17:29:34 +0500

    image

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Roboto } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "../globals.css";
 import Image from "next/image";
 import Svolve from "../(global_components)/Svolve";
 import { Toaster } from "react-hot-toast";
 import SignUp from "@/public/backgraund/signup_bg.jpg";
-import SignIn from "@/public/backgraund/signin_bg.jpg";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -29,8 +28,15 @@ export default function RootLayout({
       className={`${roboto.variable} antialiased flex items-center h-screen`}
     >
       <Toaster position="top-right" reverseOrder={true} />
-      <div className="bg-gray-200 max-w-lg w-full h-screen">
-        <Image src={SignUp} alt="Background image" width={0} height={0} className="w-full h-full object-cover" />
+      <div className="bg-gray-200 max-w-lg w-full h-screen relative">
+        <Image
+          src={SignUp}
+          alt="Background image"
+          fill
+          priority
+          className="w-full h-full object-cover"
+          quality={50}
+        />
       </div>
       <div className="max-w-xl w-full px-8">
         <Svolve />
