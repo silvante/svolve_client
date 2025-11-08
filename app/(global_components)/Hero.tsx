cdc17d7c4d8e1027dc38d9658a33877595d38575
silvante <khamidov.ko@gmail.com>
2025-11-08 23:54:06 +0500

    much better heading

--- app/(global_components)/Hero.tsx
+++ app/(global_components)/Hero.tsx
@@ -4,6 +4,13 @@ import Link from "next/link";
 import DocsAnimation from "../lottie/DocsAnimation";
 import { Play } from "lucide-react";
 import { useEffect, useState } from "react";
+// import { Montserrat } from "next/font/google";
+
+// const montserrat = Montserrat({
+//   subsets: ['latin'], // required
+//   weight: ['400', '500', '600', '700'], // choose needed weights
+//   variable: '--font-montserrat', // optional CSS variable
+// });
 
 export default function Hero() {
   const [isRegistered, setIsRejistered] = useState(false);
@@ -31,10 +38,10 @@ export default function Hero() {
             <span className="text-violet-600">organization</span> management
             platform for <span className="text-violet-600">Uzbekistan</span>
           </h1> */}
-          <h1 className="text-3xl md:text-5xl text_color text-shadow-2xs leading-16">
-            Simplifying Medical Diagnostics Management Across{" "}
-            <span className="text-violet-600">Uzbekistan</span> — Powerful,
-            Intuitive, and Efficient
+          <h1
+            className={`text-3xl md:text-6xl text_color text-shadow-2xs leading-18`}
+          >
+            Effortless Management Solutions for Medical Diagnostic Centers.
           </h1>
           <p className="w-[80%] text_color">
             Simplisity and your security metters most for us
