09414454f07e48d03c9782ef55b29d23837c0875
silvante <khamidov.ko@gmail.com>
2025-11-08 22:47:54 +0500

    Pricing

--- app/(root)/(sections)/Pricing.tsx
+++ app/(root)/(sections)/Pricing.tsx
@@ -1,17 +1,31 @@
 import Heading from "@/app/(global_components)/Heading";
+import { Check } from "lucide-react";
 
 export default function Pricing() {
   return (
     <div className="text_color space-y-4">
-        <p className="text-lg">14 Days Free Trial - No Credit Card</p>
-        <h2 className="text-5xl ">Simple & Clean Payment Method</h2>
-        <p className="text-lg">Request access today. No Credit Card Required.</p>
+      <p className="text-lg">14 Days Free Trial - No Credit Card</p>
+      <h2 className="text-5xl ">Simple & Clean Payment Method</h2>
+      <p className="text-lg">Request access today. No Credit Card Required.</p>
 
-        <div className="p-8 border border-gray-300 rounded-2xl">
-            <div>
-                <h3 className="text-5xl">99,000 UZS <span className="text-lg">per month</span></h3>
-            </div>
+      <div className="p-8 border border-gray-300 rounded-2xl space-y-5">
+        <div className="space-y-2">
+          <h3 className="text-5xl">
+            99,000 UZS <span className="text-lg">per month</span> /{" "}
+            <span className="text-lg">organization</span>
+          </h3>
+          <p className="text-lg">No hidden fees, and extra costs</p>
         </div>
+        <hr className="border-gray-300" />
+        <div className="space-y-2">
+          <p className="text-lg flex items-center gap-2">
+            <Check color="#000000" /> Request access today. No Credit Card Required.
+          </p>
+          <p className="text-lg flex items-center gap-2">
+            <Check color="#000000" /> Request access today. No Credit Card Required.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
