9060d23a2a9218f54242c5dd8662ccef2c161ee7
silvante <khamidov.ko@gmail.com>
2025-11-08 22:37:44 +0500

    Pricing

--- app/(root)/(sections)/Pricing.tsx
+++ app/(root)/(sections)/Pricing.tsx
@@ -2,8 +2,16 @@ import Heading from "@/app/(global_components)/Heading";
 
 export default function Pricing() {
   return (
-    <div>
-        <p>14 Days Free Trial - No Credit Card</p>
+    <div className="text_color space-y-4">
+        <p className="text-lg">14 Days Free Trial - No Credit Card</p>
+        <h2 className="text-5xl ">Simple & Clean Payment Method</h2>
+        <p className="text-lg">Request access today. No Credit Card Required.</p>
+
+        <div className="p-8 border border-gray-300 rounded-2xl">
+            <div>
+                <h3 className="text-5xl">99,000 UZS <span className="text-lg">per month</span></h3>
+            </div>
+        </div>
     </div>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,7 @@ const boren = Borel({
 
 export default function Home() {
   return (
-    <div className="w-full py-10">
+    <div className="w-full py-10 space-y-18">
       <Hero />
       {/* <div className="py-10 flex justify-center items-center">
         <p className={`${boren.className} text-xl`}>Community</p>
