9b51aafe46e80af19228329ff3fb3fef1771b9d0
silvante <khamidov.ko@gmail.com>
2025-11-09 21:29:13 +0500

    Lang selector res

--- app/(root)/(sections)/Pricing.tsx
+++ app/(root)/(sections)/Pricing.tsx
@@ -5,41 +5,41 @@ import Link from "next/link";
 export default function Pricing() {
   return (
     <div className="text_color space-y-4">
-      <p className="text-lg">14 Days Free Trial - No Credit Card</p>
-      <h2 className="text-5xl ">Simple & Clean Payment Method</h2>
-      <p className="text-lg">Request access today. No Credit Card Required.</p>
+      <p className="text-md md:text-lg">14 Days Free Trial - No Credit Card</p>
+      <h2 className="text-3xl md:text-5xl">Simple & Clean Payment Method</h2>
+      <p className="text-md md:text-lg">Request access today. No Credit Card Required.</p>
 
-      <div className="p-8 border border-gray-300 rounded-2xl space-y-5">
+      <div className="p-5 md:p-8 border border-gray-300 rounded-2xl space-y-5">
         <div className="space-y-2">
-          <h3 className="text-5xl">
+          <h3 className="text-3xl md:text-5xl">
             99,000 UZS <span className="text-lg">per month</span> /{" "}
             <span className="text-lg">organization</span>
           </h3>
-          <p className="text-lg">No hidden fees, and extra costs</p>
+          <p className="text-md md:text-lg">No hidden fees, and extra costs</p>
         </div>
 
         <hr className="border-gray-300" />
 
         <div className="space-y-2">
-          <p className="text-lg flex items-center gap-2">
+          <p className="text-md md:text-lg flex flex-col md:flex-row md:items-center gap-2">
             <Check color="#000000" /> Unlimited client recording.
           </p>
-          <p className="text-lg flex items-center gap-2">
+          <p className="text-md md:text-lg flex flex-col md:flex-row md:items-center gap-2">
             <Check color="#000000" /> Monthly analysis of revenue and customer
             flow.
           </p>
-          <p className="text-lg flex items-center gap-2">
+          <p className="text-md md:text-lg flex flex-col md:flex-row md:items-center gap-2">
             <Check color="#000000" /> Multy-Organization management.
           </p>
-          <p className="text-lg flex items-center gap-2">
+          <p className="text-md md:text-lg flex flex-col md:flex-row md:items-center gap-2">
             <Check color="#000000" /> Hiring unlimited workers to single
             organization.
           </p>
-          <p className="text-lg flex items-center gap-2">
+          <p className="text-md md:text-lg flex flex-col md:flex-row md:items-center gap-2">
             <Check color="#000000" /> Find every single client ever recorded
             easily with search engine.
           </p>
-          <p className="text-lg flex items-center gap-2">
+          <p className="text-md md:text-lg flex flex-col md:flex-row md:items-center gap-2">
             <Check color="#000000" /> Free support from Community and creator
             himself.
           </p>
@@ -48,7 +48,7 @@ export default function Pricing() {
         <hr className="border-gray-300" />
 
         <div className="space-y-2 flex items-start flex-col">
-          <p className="text-lg flex items-center gap-2">
+          <p className="text-md md:text-lg flex items-center gap-2">
             Pay with Visa, Mastercard, Apple Pay, Google Pay, PayPal.
           </p>
           <Link
