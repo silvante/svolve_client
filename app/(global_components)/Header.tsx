b741a2de016e2b2be3a6fefa41899ba5afb83372
silvante <khamidov.ko@gmail.com>
2025-11-09 20:30:20 +0500

    Lang selector

--- app/(global_components)/Header.tsx
+++ app/(global_components)/Header.tsx
@@ -1,11 +1,13 @@
+import LangSelector from "./LanguageSelector";
 import RegisterButton from "./RegisterButton";
 import Svolve from "./Svolve";
 
 export default function Header() {
   return (
     <header className="p-5 shadow-gray-100 flex justify-between items-center z-50">
-      <div className="h-full flex">
+      <div className="h-full flex gap-5 items-center">
         <Svolve />
+        <LangSelector />
       </div>
       <RegisterButton />
     </header>
---
+++ app/(global_components)/LanguageSelector.tsx
@@ -0,0 +1,9 @@
+export default function LangSelector() {
+  return (
+    <select className="px-3 py-2 border border-gray-300 rounded-lg">
+      <option value="en">ENG</option>
+      <option value="uz">UZB</option>
+      <option value="ru">RUS</option>
+    </select>
+  );
+}
