bbb3f166844b92313ddc62243dfdbd24b370f81b
silvante <khamidov.ko@gmail.com>
2025-11-20 23:13:19 +0500

    App is ready for test

--- app/org/[unique_name]/subscription/success/page.tsx
+++ app/org/[unique_name]/subscription/success/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { useParams, useSearchParams } from "next/navigation";
+import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function SubscriptionSuccess() {
