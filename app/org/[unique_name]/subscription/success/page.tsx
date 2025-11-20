8fa90d7bcc8dc77a91344122b38b537fc88a39e2
silvante <khamidov.ko@gmail.com>
2025-11-20 23:12:27 +0500

    Success payment page is running and beautifully

--- app/org/[unique_name]/subscription/success/page.tsx
+++ app/org/[unique_name]/subscription/success/page.tsx
@@ -1,8 +1,49 @@
+"use client";
+import Link from "next/link";
+import { useParams, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function SubscriptionSuccess() {
+  const params = useParams();
+  const unique_name = params.unique_name;
+
+  const [sec, setSec] = useState(60);
+  const [free, setFree] = useState(false);
+
+  useEffect(() => {
+    if (sec <= 0) {
+      setFree(true);
+      return;
+    }
+
+    const interval = setInterval(() => {
+      setSec((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [sec]);
+
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <div className="flex flex-col gap-2 items-center justify-center">
-        <p className="text_color text-xl">Processing your payment pleace wait...!</p>
+      <div className="flex flex-col gap-4 items-center justify-center">
+        <p className="text_color text-2xl">
+          {free
+            ? "Thank you for subscription, your organization is ready!"
+            : "Processing your payment pleace wait...!"}
+        </p>
+        {free ? (
+          <Link
+            href={`/org/${unique_name}/validation`}
+            className="py-3 px-7 rounded-lg bg-violet-600 text-white"
+          >
+            Continue
+          </Link>
+        ) : (
+          <p>
+            <span className="text-violet-600 text-2xl">{sec}</span> second
+            remaining
+          </p>
+        )}
       </div>
     </div>
   );
