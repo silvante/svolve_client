8027e60c87ef9de891e69559f7b15385e4975c29
silvante <khamidov.ko@gmail.com>
2025-11-28 21:15:32 +0500

    IDK how to help

--- app/(global_components)/Hero.tsx
+++ app/(global_components)/Hero.tsx
@@ -9,18 +9,18 @@ import { useTranslations } from "next-intl";
 export default function Hero() {
   const t = useTranslations();
 
-  // const [isRegistered, setIsRejistered] = useState(false);
-  // const [loading, setLoading] = useState(true);
-  // useEffect(() => {
-  //   const access_token = localStorage.getItem("access_token");
-  //   const reset_token = localStorage.getItem("reset_token");
-  //   if (reset_token && access_token) {
-  //     setIsRejistered(true);
-  //     setLoading(false);
-  //   } else {
-  //     setLoading(false);
-  //   }
-  // }, []);
+  const [isRegistered, setIsRejistered] = useState(false);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const access_token = localStorage.getItem("access_token");
+    const reset_token = localStorage.getItem("reset_token");
+    if (reset_token && access_token) {
+      setIsRejistered(true);
+      setLoading(false);
+    } else {
+      setLoading(false);
+    }
+  }, []);
 
   return (
     <div className="w-full space-y-5">
@@ -33,12 +33,12 @@ export default function Hero() {
             {t("hero.title")}
           </h1>
           <p className="w-[80%] text_color">{t("hero.desc")}</p>
-          {/* <Link
+          <Link
             href={isRegistered ? "/panel" : "/signin"}
             className="py-2 px-4 font-medium flex gap-2 items-center text-lg  text-white bg-violet-600 rounded-xl"
           >
             {t("hero.start_button")} <Play />
-          </Link> */}
+          </Link>
         </div>
         <div className="flex-1 flex justify-end">
           <DocsAnimation />
