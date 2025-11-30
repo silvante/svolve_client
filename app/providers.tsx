c479bb94b89168172e5cf905401da31ed5462657
silvante <khamidov.ko@gmail.com>
2025-11-30 19:22:04 +0500

    Ok I won't

--- app/providers.tsx
+++ app/providers.tsx
@@ -12,7 +12,6 @@ interface Params {
 const timeZone = "Asia/Tashkent";
 
 export default function LangProvider({ lang, messages, children }: Params) {
-  // should I get rid of multi language support?
   return (
     <NextIntlClientProvider locale={lang} messages={messages} timeZone={timeZone}>
       {children}
