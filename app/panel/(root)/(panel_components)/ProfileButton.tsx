182dbbced8f69910a1593d42435732f40789270f
silvante <khamidov.ko@gmail.com>
2025-12-05 12:23:52 +0500

    fixing small issues

--- app/panel/(root)/(panel_components)/ProfileButton.tsx
+++ app/panel/(root)/(panel_components)/ProfileButton.tsx
@@ -1,7 +1,8 @@
 "use client"
 import Spinner from "@/app/(global_components)/Spinner";
-import { Menu, UserCircle } from "lucide-react"
+import { Menu } from "lucide-react"
 import { useSelector } from "react-redux";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function ProfileButton() {
     const { currentUser, loading } = useSelector((state: any) => state.user);
@@ -12,15 +13,12 @@ export default function ProfileButton() {
     return (
         <div className="flex gap-2 rounded-full border border-gray-400 justify-center items-center p-1 pr-3 cursor-pointer">
             <div className="w-8 h-8 rounded-full bg-gray-300 overflow-hidden flex justify-center items-center text-gray-500">
-                {currentUser.avatar ? (
-                    <img
-                        src={currentUser.avatar}
-                        alt="Your avatar"
-                        className="w-full h-full aspect-square object-cover"
-                    />
-                ) : (
-                    <UserCircle />
-                )}
+                <Avatar className="w-8 h-8  ">
+                    <AvatarImage src={currentUser.avatar} />
+                    <AvatarFallback>
+                        {currentUser.name.split("")[0].toUpperCase()}
+                    </AvatarFallback>
+                </Avatar>
             </div>
             <Menu />
         </div>
